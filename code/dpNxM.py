213238ee5b32141369dc589dcc37a97716bbbecb
Alejandra <amonzon1113@gmail,com>
2023-04-09 17:17:57 -0400

    codigo

--- code/dpNxM.py
+++ code/dpNxM.py
@@ -1,19 +1,21 @@
 #Metodo dp
 def ppp(S,T):
+    #Aquise pueden detectar casillos O(n) comprobando que las letras de T sean subconjunto de las de S
+    # Que len de T <= que len S
+
+
     n = len(S)
     m = len(T)
-    if n> m: return 0
-
     prefix = compute_prefix(S,T)
     count = 0
-
-    if T.__contains__(S[0]):
+    
+    if T.__contains__(S[0]):#Aqui hay que ver bien que hacer, pero con buscar cuantos sufizo de T se pueden formas tal 
+        #que el ultimo movimiento sea poner la letra hacia atras y luego combinar con una mtriz como la del else.
         sufix = compute_sufix(S, T)
         count= sufix[0][m-1] * (n - m + 1)
-        
         for i in range(1,m):           
             count += sufix[i][m-1+i]* prefix[m][i-1]
-    
+    #Si la primera letra no pertenece al prefijo(Puede extenderse a que en S haya un caracter que no pertenezca a T antes que aparezca en S el ultimo caracter de T)        
     return count + int(sum((prefix[i][-1] - prefix[i+1][-1])* (2**(max(i-1,0))) *(T[-1]==S[i]) for i in range(n))*2)
 
 def compute_prefix(S,T):
@@ -32,30 +34,34 @@ def compute_prefix(S,T):
 
     return dp
     
- def compute_sufix(S,T):
-    m = len(T)
-    dp = [[0 for j in range(2*m)] for i in range(2*m)]
-    tmdp = [[0 for j in range(2*m)] for i in range(2*m)]
+def compute_sufix(S,T):
+        m = len(T)
+        dp = [[0 for j in range(2*m)] for i in range(2*m)]
+        tmdp = [[0 for j in range(2*m)] for i in range(2*m)]
+                    
+        for i in range(2*m):
+            if i >= m or T[i] == S[0]:
+                tmdp[i][i] = 2
+                
+        for k in range(1, m):    
+            c = S[k]
+            i = 0        
             
-    for i in range(2*m):
-        if i >= m or T[i] == S[0]:
-            tmdp[i][i] = 2
-        
-    for k in range(1, m):    
-        c = S[k]
-        i = 0        
-    
-        for j in range(k, m  k):
-            if j >= m:
-                dp[i][j] += dp[i][j-1]
-                tmdp[i][j] += tmdp[i][j-1]          
-        
-            if j< m and c==T[j]:
-                dp[i][j] += tmdp[i][j-1]
-                tmdp[i][j] += tmdp[i][j-1]
-        
-            if i >= m or c == T[i]:
-                dp[i][j] += dp[i+1][j]
-                tmdp[i][j] += tmdp[i+][j]
-            i += 1  
-    return dp  
+            for j in range(k, m + k):
+
+                if j >= m:
+                    dp[i][j] += dp[i][j-1]
+                    tmdp[i][j] += tmdp[i][j-1]          
+                
+                if j< m and c==T[j]:
+                    dp[i][j] += tmdp[i][j-1]
+                    tmdp[i][j] += tmdp[i][j-1]
+                
+                if i >= m or c == T[i]:
+                    dp[i][j] += dp[i+1][j]
+                    tmdp[i][j] += tmdp[i+1][j]
+
+                i += 1  
+        return dp  
+
+print(ppp("ccbb", "bbc"))
